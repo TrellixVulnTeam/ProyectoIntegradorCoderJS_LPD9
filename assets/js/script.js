24c91c4deaf34624106234edb8f139229533e12e
Diego Suarez <31140727+diegosuarez07@users.noreply.github.com>
2022-06-10 20:25:26 -0300

    LOGIN REALIZADO CON FUNCION DE ORDEN SUPERIOR SOME , APLICANDO ARREGLOS DE OBJETOS TAMBIEN

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,70 @@
-class Usuario{
-    constructor(correo, contraseña){
-        this.correo = correo
-        this.contraseña = contraseña
-    }
-}
+const usuarios = [{
+    id: 1,
+    nombre: 'diego',
+    apellido: 'suarez',
+    edad: 25,
+    correo: 'diego@gmail.com',
+    contraseña: '1234'
+},
+{
+    id: 2,
+    nombre: 'tomas',
+    apellido: 'lopez',
+    edad: 31,
+    correo: 'tomas@gmail.com',
+    contraseña: '1990'
+},
+{
+    id:3,
+    nombre: 'eugenia',
+    apellido: 'balmaceda',
+    edad: 20,
+    correo: 'eugenia@gmail.com',
+    contraseña: '1445'
+},
+{
+    id:4,
+    nombre: 'georgina',
+    apellido: 'velez',
+    edad: 38,
+    correo: 'georgina@gmail.com',
+    contraseña: '1667'
+},
+{
+    id:5,
+    nombre: 'ruben',
+    apellido: 'toranzo',
+    edad: 29,
+    correo: 'ruben@gmail.com',
+    contraseña: '1330'
+}]
+
+
+
+
 const correoUsuario = document.getElementById("correo")
 const contraseñaUsuario = document.getElementById("contraseña")
-const arrayUsuarios = []
 
 
 
-function cargarUsuario(){
+
+function validarUsuario(){
+    debugger
     if(correoUsuario.value != "" && contraseñaUsuario.value != ""){
-        let usuarioNuevo = new Usuario(correoUsuario.value, contraseñaUsuario.value) 
-        arrayUsuarios.push(usuarioNuevo)
+        
+        const validar = usuarios.some(usuario => usuario.correo === correoUsuario.value && usuario.contraseña === contraseñaUsuario.value)
+
+        if(validar){
+            alert("Usuario encontrado. Acceso exitoso")
+            window.location = "/assets/pages/home.html"
+        }else{
+            alert("Usuario no encontrado.")
+        }
+    
         limpiarCampos()
+    
+    }else{
+        alert("Debe completar los campos correo y contraseña. No pueden estar vacios")
     }
 }
 
